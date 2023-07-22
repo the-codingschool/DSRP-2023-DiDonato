e38795c11965286f0d63f66de9796b14b6918a7d
Vincent Sanchez <imvincentsanchez@gmail.com>
2023-07-21 19:41:35 -0700

    cleaned datasets

--- Vincent Sanchez/scripts/cleaningData.R
+++ Vincent Sanchez/scripts/cleaningData.R
@@ -1,3 +1,6 @@
+
+# read in data files
+
 library(dplyr)
 demographics <- read.csv("data/data.csv")
 
@@ -13,44 +16,26 @@ med_conditions <- read.xport("Vincent Sanchez/more-data/P_MCQ.XPT")
 
 # SEQN: Respondent sequence number
 
-# SDDSRVYR: Data release cycle
 # RIAGENDR: Gender of the participant
 # RIDAGEYR: Age in years of the participant (max 80)
 # RIDRETH1: Reported race and Hispanic origin information
 # RIDRETH3: Reported race and Hispanic origin information, with Non-Hispanic Asian Category
-# DMDBORN4: Birth country
-# DMDEDUC2: Highest grade or level of school completed or the highest degree received
 # DMDMARTZ: Marital Status
 
 # BMXWT: Weight in kg
 # BMXHT: Standing height in cm
 # BMXBMI: Body Mass Index (kg/m^2)
-# BMDBMIC: BMI Category (children/youth)
 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+# BPQ030: Told had high blood pressure 2+ times
+# BPD035: Age told had hypertension
+# BPQ040A: Taking prescription for hypertension
 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+# MCQ160L: Ever told you had any liver condition
+# MCQ170L: Do you still have a liver condition
+# MCD180L: Age when told you had a liver condition
+# MCQ510A: Liver condition - Fatty liver
+# MCQ510C: Liver condition - Liver cirrhosis
+# MCQ510D: Liver condition - Viral hepatitis
 
 
 ### Data Cleaning ####
@@ -60,30 +45,39 @@ med_conditions <- read.xport("Vincent Sanchez/more-data/P_MCQ.XPT")
 
 # demographics cleaning
 head(demographics)
-demo_clean <- select(demographics, SEQN, DMDBORN4, DMDEDUC2, DMDMARTZ, RIAGENDR, RIDAGEYR, RIDRETH1, RIDRETH3, SDDSRVYR)
-demo_clean <- rename(demo_clean, cycle = SDDSRVYR, gender = RIAGENDR, age = RIDAGEYR, birth_country = DMDBORN4, education = DMDEDUC2, 
-                     marital_status = DMDMARTZ, race = RIDRETH1, race_withasian = RIDRETH3)
-View(demo_clean)
+demo_clean <- select(demographics, SEQN, DMDMARTZ, RIAGENDR, RIDAGEYR, RIDRETH1, RIDRETH3)
+demo_clean <- rename(demo_clean, gender = RIAGENDR, age = RIDAGEYR, marital_status = DMDMARTZ, 
+                     race = RIDRETH1, race_withasian = RIDRETH3)
 
 # body measurements cleaning
 head(body_measures)
-bm_clean <- select(body_measures, SEQN, BMXWT, BMXHT, BMXBMI, BMDBMIC)
-bm_clean <- rename(bm_clean, weight_kg = BMXWT, height_cm = BMXHT, bmi = BMXBMI, bmi_category = BMDBMIC)
-View(bm_clean)
+bm_clean <- select(body_measures, SEQN, BMXWT, BMXHT, BMXBMI)
+bm_clean <- rename(bm_clean, weight_kg = BMXWT, height_cm = BMXHT, bmi = BMXBMI)
 
 general_info <- inner_join(demo_clean, bm_clean)
 View(general_info)
 
-# blood pressure & cholesterol cleaning
+# blood pressure & cholesterol cleaning (only want blood pressure)
 head(bp_chol)
+hbp <- select(bp_chol, SEQN, BPQ030, BPD035, BPQ040A)
+hbp <- rename(hbp, told_hbp = BPQ030, agetold_hbp = BPD035, prescr_hbp = BPQ040A)
 
-# medical conditions cleaning
+View(hbp)
+
+# medical conditions cleaning (only want liver conditions)
 head(med_conditions)
-medconditions_clean <- select(med_conditions, SEQN, MCQ160M, MCQ170M, MCD180M, MCQ160L, MCQ170L, MCD180L, MCQ500, MCQ510A, MCQ510B, MCQ510C, 
-                              MCQ510D, MCQ510E, MCQ510F)
+liver_info <- select(med_conditions, SEQN, MCQ160L, MCQ170L, MCD180L, MCQ510A, MCQ510C, 
+                              MCQ510D)
+liver_info <- rename(liver_info, fatty_liver = MCQ510A, liver_cirrhosis = MCQ510C,
+                           viral_hepa = MCQ510D, told_liver = MCQ160L, stillhave_liver = MCQ170L, agetold_liver = MCD180L)
+View(liver_info)
 
-View(medconditions_clean)
+# combine all datasets into one
+everything <- inner_join(inner_join(general_info, hbp), liver_info)
 
+# filter out NAs in important variables
+livercond_hbp <- filter(everything, !is.na(told_liver))
+livercond_hbp <- filter(livercond_hbp, !is.na(told_hbp), !is.na(prescr_hbp), !is.na(height_cm), !is.na(bmi))
+View(livercond_hbp)
 
-
-
+# initial dataset is now ready for analyzing (livercond_hbp)
