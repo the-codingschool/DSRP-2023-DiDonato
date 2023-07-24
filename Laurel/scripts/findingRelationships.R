c4911b7833dffcdad848d78bb4120a171b01923c
Laurel Sun <laurel.sun.06@gmail.com>
2023-07-24 11:01:40 -0700

    More plots

--- Laurel/scripts/findingRelationships.R
+++ Laurel/scripts/findingRelationships.R
@@ -14,3 +14,31 @@ ggplot(data = health_overview_plot, aes(x = depressed, y = bmi)) +
   labs(y = "Body mass index", x = "Depressed", caption = 
          "0 = not at all, 1 = several days, 2 = more than half the days, 3 = nearly every day") +
   theme_bw()
+
+ggplot(data = health_overview_plot, aes(x = depressed, y = income_to_poverty)) +
+  geom_jitter(width = 0.25, alpha = 0.2, color = "blue") +
+  labs(y = "Ratio of income to poverty", x = "Depressed", caption = 
+         "0 = not at all, 1 = several days, 2 = more than half the days, 3 = nearly every day") +
+  theme_bw()
+
+ggplot(data = health_overview_plot, aes(x = depressed, y = monthly_poverty_index)) +
+  geom_jitter(width = 0.25, alpha = 0.2, color = "blue") +
+  labs(y = "Monthly poverty index", x = "Depressed", caption = 
+         "0 = not at all, 1 = several days, 2 = more than half the days, 3 = nearly every day") +
+  theme_bw()
+
+ggplot(data = health_overview_plot, aes(x = depressed, y = education)) +
+  geom_jitter(width = 0.25, alpha = 0.2, color = "blue") +
+  labs(y = "Education level", x = "Depressed", caption = 
+         "0 = not at all, 1 = several days, 2 = more than half the days, 3 = nearly every day") +
+  theme_bw()
+
+ggplot(data = health_overview_plot, aes(x = depressed, y = age)) +
+  geom_jitter(width = 0.25, alpha = 0.2, color = "blue") +
+  labs(y = "Age", x = "Depressed", caption = 
+         "0 = not at all, 1 = several days, 2 = more than half the days, 3 = nearly every day") +
+  theme_bw()
+
+
+
+
