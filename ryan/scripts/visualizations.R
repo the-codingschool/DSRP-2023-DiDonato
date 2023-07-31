da4edd810d0314465ea093a2a0d6f4eeb963a3b4
Ryan Wang <ryanruimao@gmail.com>
2023-07-31 12:51:46 -0700

    added some new visuals.

--- ryan/scripts/visualizations.R
+++ ryan/scripts/visualizations.R
@@ -7,53 +7,61 @@ Cors <- NAtoMean |>
 ggplot(Cors, aes(x=Var1, y=Var2, fill = value)) +
   geom_tile() +
   scale_fill_gradient2(low = "red", high = "blue", mid = "white",
-                       midpoint = 0)
-# 
-# 
-# #ggplot(NAtoMean, aes(x= LBDHDD, y = LBXGH, color = RIDRETH3)) +
-# #  geom_point()
-# 
-highCorrelation <- filter(Cors, (value >= 0.1 | value <= -0.1) & 
-                            Var1 == "ever_told_you_had_coronary_heart_disease")
-# 
-# numDaysOfExercise <- select(exerciseActivity, PAQ610)
-# 
-# ggplot(merged_data, aes(x= MCQ160C)) +
-#   geom_bar()
-# 
-# ggplot(merged_data, aes(x= PAQ610, fill = as.factor(MCQ160C))) +
-#   geom_histogram(bins=10, binwidth = 1) + labs(x= "Days of Vigorous Exercise a Week", 
-#                                                fill= "Diagonsed with Heart Disease? (1 = yes, 2 = no, 9= refused)")
-# 
-# ggplot(merged_data, aes(x=PAQ610)) +
-#   geom_histogram()
+                       midpoint = 0) +
+  theme(axis.text.x = element_text(angle = 90, vjust =0.5, hjust = 1))
 
-ggplot(NAtoMean, aes(x = ldl_cholesterol_friedewald_mg_d_l)) +
+
+
+  
+
+
+highCorrelation <- filter(Cors, (value >= 0.05 | value <= -0.05) & 
+                            Var1 == "had_coronary_heart_disease")
+
+
+ggplot(gather(exerciseActivity, "Key", "Value", -seqn), aes(Value)) +
+  geom_histogram() +
+  facet_wrap(~Key, scales='free_x')
+
+ggplot(gather(medicalConditions, "Key", "Value", -seqn), aes(Value)) +
+  geom_histogram() +
+  facet_wrap(~Key, scales='free_x')
+
+ggplot(gather(demographics, "Key", "Value", -seqn), aes(Value)) +
+  geom_histogram() +
+  facet_wrap(~Key, scales='free_x')
+
+
+
+ggplot(exerciseActivity, aes(x= walk_or_bike)) +
+  geom_histogram(bins = 2)
+
+ggplot(NAtoMean, aes(x = ldl_cholesterol_mg_d_l)) +
   geom_histogram()
 
 ggplot(NAtoMean, aes(x=  age_at_screening_yrs, 
-                     y= ever_told_you_had_coronary_heart_disease)) +
+                     y= had_coronary_heart_disease)) +
   geom_point()
 
 
 ggplot(NAtoMean, aes(x=bmi, 
-                     y= ever_told_you_had_coronary_heart_disease)) +
+                     y= had_coronary_heart_disease)) +
   geom_point()
 
-ggplot(NAtoMean, aes(x= systolic_2nd_reading, 
-                     y= ever_told_you_had_coronary_heart_disease))   +
+ggplot(NAtoMean, aes(x= mean_systolic, 
+                     y= had_coronary_heart_disease))   +
   geom_point()
 
-ggplot(CAD, aes(x= age_at_screening_yrs, fill = ever_told_you_had_coronary_heart_disease)) +
+ggplot(CAD, aes(x= age_at_screening_yrs, fill = had_coronary_heart_disease)) +
   geom_histogram(bins = 40)
 
-ggplot(merged_data, aes(x= ever_had_pain_or_discomfort_in_chest, 
-                        fill= as.factor(ever_told_you_had_coronary_heart_disease))) +
+ggplot(merged_data, aes(x= discomfort_in_chest, 
+                        fill= as.factor(had_coronary_heart_disease))) +
   geom_histogram()
 
-ggplot(merged_data, aes(x= shortnes_of_breath_on_stairs, 
-                        fill= as.factor(ever_told_you_had_coronary_heart_disease))) +
+ggplot(merged_data, aes(x= shortnes_of_breath_stairs, 
+                        fill= as.factor(had_coronary_heart_disease))) +
   geom_histogram()
 
-ggplot(NAtoMean, aes(x= typical_time_spent_doing_moderate_work_a_day_min )) +
+ggplot(NAtoMean, aes(x= moderate_work_a_day_min )) +
   geom_histogram(bins = 50)
