95378a3ebf79bb1e78c80a4f6afd110a28d8d6ff
Laurel Sun <laurel.sun.06@gmail.com>
2023-07-31 09:15:48 -0700

    plotting the error

--- Laurel/scripts/bioClassModel.R
+++ Laurel/scripts/bioClassModel.R
@@ -48,14 +48,57 @@ boost_predicted <- class_results_bio$boost_pred
 forest_predicted <- class_results_bio$forest_pred
 actual <- class_results_bio$depressed
 
-confusionMatrix(boost_predicted, actual,
+boost_matrix_bio <- confusionMatrix(boost_predicted, actual,
                 mode = "everything",
                 positive="1")
 
-confusionMatrix(forest_predicted, actual,
+forest_matrix_bio <- confusionMatrix(forest_predicted, actual,
                 mode = "everything",
                 positive="1")
 
+## modelling the distribution of what the models got right and wrong
+select(class_results_bio, depressed, boost_pred, forest_pred)
+
+## boosted tree model
+boost_data_frame_bio <- data.frame(boost_matrix_bio$table)
+
+'          Reference
+Prediction    0    1    2    3
+         0 2501  342   61   26
+         1  106  209   58   35
+         2    6   20   14   12
+         3   11   12   13   43
+0 % acc: 2501/2624 = 95%
+1 % acc: 209/583 = 36%
+2 % acc: 14/146 = 10%
+3 % acc: 43/116 = 37%'
 
 
+## random forest model
+forest_data_frame_bio <- data.frame(forest_matrix_bio$table)
+
+'          Reference
+Prediction    0    1    2    3
+         0 2505  360   63   28
+         1  106  202   65   39
+         2    2   11    8    3
+         3   11   10   10   46
+0 % acc: 2504/2624 = 95%
+1 % acc: 202/583 = 35%
+2 % acc: 8/146 = 5.5%
+3 % acc: 46/116 = 40%'
+
+## boost tree error graph
+ggplot(boost_data_frame_bio, aes(fill=Prediction, y=Freq, x=Reference)) + 
+  geom_bar(position="fill", stat="identity") +
+  theme_bw() +
+  labs(y = "Percentage") +
+  scale_fill_manual(values=wes_palette(n=4, name="Moonrise1"))
+
+## forest tree error graph
+ggplot(forest_data_frame_bio, aes(fill=Prediction, y=Freq, x=Reference)) + 
+  geom_bar(position="fill", stat="identity") +
+  theme_bw() +
+  labs(y = "Percentage") +
+  scale_fill_manual(values=wes_palette(n=4, name="Moonrise1"))
 
--- Laurel/scripts/socialClassModel.R
+++ Laurel/scripts/socialClassModel.R
@@ -7,6 +7,8 @@ library(parsnip)
 library(yardstick)
 library(Metrics)
 library(caret)
+library(RColorBrewer)
+library(wesanderson)
 
 set.seed(7262023)
 
@@ -48,18 +50,67 @@ forest_predicted <- class_results_social$forest_pred
 actual <- class_results_social$depressed
 
 # create confusion matrix
-confusionMatrix(boost_predicted, actual,
+boost_matrix_social <- confusionMatrix(boost_predicted, actual,
                 mode = "everything",
                 positive="1")
 
-confusionMatrix(forest_predicted, actual,
+forest_matrix_social <- confusionMatrix(forest_predicted, actual,
                 mode = "everything",
                 positive="1")
 
 ## modelling the distribution of what the model got right and wrong
+select(class_results_social, depressed, boost_pred, forest_pred)
 
+## boosted tree model
+'          Reference
+Prediction    0    1    2    3
+         0 2503  321   60   34
+         1   92  275   44   24
+         2    3    6   31    8
+         3    2    9    6   51
+0 % acc: 2503/2600 = 96%
+1 % acc: 275/611 = 45%
+2 % acc: 31/141 = 22%
+3 % acc: 51/117 = 44%'
+boost_data_frame_social <- data.frame(boost_matrix_social$table)
+boost_table_social <- pivot_wider(boost_data_frame_social,
+            names_from = Reference,
+            values_from = Freq)
 
+## random forest model
+'          Reference
+Prediction    0    1    2    3
+         0 2554  326   62   37
+         1   44  276   38   23
+         2    0    5   39    2
+         3    2    4    2   55
+0 % acc: 2554/2600 = 98%
+1 % acc: 276/611 = 45%
+2 % acc: 39/141 = 28%
+3 % acc: 55/117 = 47%'
+forest_data_frame_social <- data.frame(forest_matrix_social$table)
+forest_table_social <- pivot_wider(forest_data_frame_social,
+            names_from = Reference,
+            values_from = Freq)
 
+## modelling the error of the boost tree
+ggplot(boost_data_frame_social, aes(fill=Prediction, y=Freq, x=Reference)) + 
+  geom_bar(position="dodge", stat="identity") +
+  theme_bw()
 
+ggplot(boost_data_frame_social, aes(fill=Prediction, y=Freq, x=Reference)) + 
+  geom_bar(position="stack", stat="identity") +
+  theme_bw()
 
+ggplot(boost_data_frame_social, aes(fill=Prediction, y=Freq, x=Reference)) + 
+  geom_bar(position="fill", stat="identity") +
+  theme_bw() +
+  labs(y = "Percentage") +
+  scale_fill_manual(values=wes_palette(n=4, name="Moonrise1"))
 
+## modelling the error of the random
+ggplot(forest_data_frame_social, aes(fill=Prediction, y=Freq, x=Reference)) + 
+  geom_bar(position="fill", stat="identity") +
+  theme_bw() +
+  labs(y = "Percentage") +
+  scale_fill_manual(values=wes_palette(n=4, name="Moonrise1"))
